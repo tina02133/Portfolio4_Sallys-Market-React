3e312c031b279437c59e81b6a17caba776d66adf
Siyeon Leem <tina980213@gmail.com>
2023-05-01 15:31:38 +0900

    리액트쇼핑몰

--- OneDrive/바탕 화면/리액트장인/shop/src/App.js
+++ OneDrive/바탕 화면/리액트장인/shop/src/App.js
@@ -1,71 +1,100 @@
 import logo from './logo.svg';
 import './App.css';
-import {Button, Navbar, Container,Nav} from 'react-bootstrap';
+import { Button, Navbar, Container, Nav } from 'react-bootstrap';
 import data from './data.js';
 import { useState } from 'react';
-import {Routes, Route, Link } from 'react-router-dom'
+import { Routes, Route, Link, useNavigate, Outlet } from 'react-router-dom'
 import Detail from './detail';
 
-function App(){ 
+function App() {
 
   // import한 상품 data state에 저장하기
   let [shoe] = useState(data);
-  
+  let navigate = useNavigate();
+
 
   return (
     <div className="App">
-       <Navbar bg="dark" variant="dark">
+      <Navbar bg="dark" variant="dark">
         <Container>
           <Navbar.Brand href="#home">ShoeShop</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Cart</Nav.Link>
+            <Nav.Link onClick={()=>{navigate('/')}}>Home</Nav.Link>
+            <Nav.Link onClick={()=>{navigate('/detail')}}>Detail</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
-      {/* Link 사용하여 버튼 클릭 시 원하는 페이지로 이동하기 */}
-      <Link to="/">메인페이지</Link>
-      <Link to="/detail">상세페이지</Link>
       {/* Router 사용하여 페이지 나누기  */}
       <Routes>
         <Route path="/" element={
           <>
-             <div className="main-bg"></div>
-             <div className="container">
-               <div className="row">
-                 {/* map 반복문 사용 시 {} 로 묶어주기 
+            <div className="main-bg"></div>
+            <div className="container">
+              <div className="row">
+                {/* map 반복문 사용 시 {} 로 묶어주기 
                      ()안의 a는 해당 반복회차의  shoe 데이터를 의미.
                      i는 반복하며 올라가는 고유의 수 ex) 0, 1, 2
                  */}
-                 {
-                   shoe.map(function(a, i){
-                   return(
-                     <Product shoe = {shoe[i]} i = {i}/>
-                   )
-                 })
-               }
-               </div>
-             </div>
+                {
+                  shoe.map(function (a, i) {
+                    return (
+                      <Product shoe={shoe[i]} i={i} />
+                    )
+                  })
+                }
+              </div>
+            </div>
           </>
         }></Route>
-        <Route path='/detail' element={
-          <Detail/>
-          }>
+        {/* detail 상세페이지 여러개 만들때 :id 파라미터 이용하기 */}
+        <Route path='/detail/:id' element={<Detail data = {data}/>}></Route>
+        {/* 404페이지 */}
+        <Route path='*' element={<div>없는 페이지입니다</div>}></Route>
+        {/* nested routes */}
+        <Route path='/about' element={<About/>}>
+          <Route path='tel' element={<div>연락처 정보입니다</div>}></Route>
+          <Route path='location' element={<div>위치 정보입니다</div>}></Route>
+        </Route>
+        <Route path='/event' element={<Event/>}>
+           <Route path='one' element={<h4>첫 주문 시 양배추즙 서비스</h4>}></Route>
+           <Route path='two' element={<h4>생일 기념 쿠폰</h4>}></Route>
         </Route>
       </Routes>
     </div>
   );
 }
 
-// 상품목록 컴포넌트 만들기 
-function Product(props){
+//About => 회사 정보 컴포넌트 
+function About(){
+  return (
+    <>
+      <h3>회사 소개페이지입니다</h3>
+      <Outlet></Outlet>
+    </>
+  )
+
+}
+
+//Event => 이벤트 정보 컴포넌트 만들기
+function Event(){
   return(
+    <>
+      <h3>오늘의 이벤트</h3>
+      <Outlet></Outlet>
+    </>
+  )
+}
+
+
+// 상품목록 컴포넌트 만들기 
+function Product(props) {
+  return (
     <div className="col-md-4">
-    <img src={"https://codingapple1.github.io/shop/shoes"+ (props.i+1) + ".jpg"} width="80%"/>
-    <h4>{props.shoe.title}</h4>
-    <p>{props.shoe.content}</p>
-    <p>{props.shoe.price}</p>
-  </div>
+      <img src={"https://codingapple1.github.io/shop/shoes" + (props.i + 1) + ".jpg"} width="80%" />
+      <h4>{props.shoe.title}</h4>
+      <p>{props.shoe.content}</p>
+      <p>{props.shoe.price}</p>
+    </div>
   )
 }
 
--- OneDrive/바탕 화면/리액트장인/shop/src/detail.js
+++ OneDrive/바탕 화면/리액트장인/shop/src/detail.js
@@ -1,19 +1,59 @@
-function Detail(){
-    return (
-        <div className="container">
-  <div className="row">
-    <div className="col-md-6">
-      <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" />
-    </div>
-    <div className="col-md-6">
-      <h4 className="pt-5">상품명</h4>
-      <p>상품설명</p>
-      <p>120000원</p>
-      <button className="btn btn-danger">주문하기</button> 
-    </div>
-  </div>
-</div> 
-    )
-}
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom"
 
+function Detail(props) {
+
+  // 경로에서 param 네이밍을 id 로 했으니, 변수명을 동일하게 통일해주어야 함 
+  let { id } = useParams();
+  // let num = props.data[id].id;
+
+  let 찾은상품 = props.data.find(function (x) {
+    return x.id == id;
+  })
+
+  // 렌더링 2초 이후 div 사라지는 동적인 ui 구현을 위해 state 에 ui 상태값 넣어놓기
+  let[박스상태, 박스상태변경] = useState(true);
+
+
+  // component 의 라이프사이클 
+  // mount / update 시마다 실행될 코드
+  // 이부분에선 return 함수 사용할 필요 없음, 그냥 단순히 '박스상태변경' 이라는 함수 실행하고 끝이니까.
+  useEffect(()=>{
+      // 렌더링 2초 이후 div 사라지는 코드 
+      let a = setTimeout(()=>{     
+           박스상태변경(false);        
+      },3000,[])
+
+      // clear up function
+      // ==> mount 시 실행되는 것이 아닌 unmount 시 실행됨 
+      return ()=>{
+        clearTimeout(a)
+      } 
+
+  });
+
+  return (
+    <div className="container">
+      
+      {
+        박스상태 == true 
+        ? <div className="alert alert-warning">
+        <p>2초 이내 구매 시 10 % 할인</p>
+      </div> 
+      : null
+      }
+      <div className="row">
+        <div className="col-md-6">
+          <img src={'https://codingapple1.github.io/shop/shoes' + (찾은상품.id + 1) + '.jpg'} width="80%" />
+        </div>
+        <div className="col-md-6">
+          <h4 className="pt-5">{찾은상품.title}</h4>
+          <p>{찾은상품.content}</p>
+          <p>{찾은상품.price}</p>
+          <button className="btn btn-danger">주문하기</button>
+        </div>
+      </div>
+    </div>
+  )
+}
 export default Detail
