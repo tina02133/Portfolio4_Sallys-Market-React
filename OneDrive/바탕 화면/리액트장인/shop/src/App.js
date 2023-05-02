e92d195cab1b6320b72d350835f80335e2b3581d
Siyeon Leem <tina980213@gmail.com>
2023-05-03 00:21:38 +0900

    리액트

--- OneDrive/바탕 화면/리액트장인/shop/src/App.js
+++ OneDrive/바탕 화면/리액트장인/shop/src/App.js
@@ -5,13 +5,19 @@ import data from './data.js';
 import { useState } from 'react';
 import { Routes, Route, Link, useNavigate, Outlet } from 'react-router-dom'
 import Detail from './detail';
+import axios from 'axios'
 
 function App() {
 
   // import한 상품 data state에 저장하기
-  let [shoe] = useState(data);
+  let [shoe, shoe변경] = useState(data);
   let navigate = useNavigate();
 
+  // 더보기 버튼 클릭 횟수 담을 state 
+  // 클릭 횟수에 따라 ajax 요청할 데이터 구분 위해서.
+  // 3번 클릭 시, 더이상 보여줄 상품 없다고 알림 띄우기
+
+  let [클릭, 클릭변경] = useState(0);
 
   return (
     <div className="App">
@@ -19,8 +25,8 @@ function App() {
         <Container>
           <Navbar.Brand href="#home">ShoeShop</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link onClick={()=>{navigate('/')}}>Home</Nav.Link>
-            <Nav.Link onClick={()=>{navigate('/detail')}}>Detail</Nav.Link>
+            <Nav.Link onClick={() => { navigate('/') }}>Home</Nav.Link>
+            <Nav.Link onClick={() => { navigate('/detail') }}>Detail</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
@@ -38,34 +44,48 @@ function App() {
                 {
                   shoe.map(function (a, i) {
                     return (
-                      <Product shoe={shoe[i]} i={i} />
+                      <Product onClick={() => { navigate('/detail/' + i) }} shoe={shoe[i]} i={i} />
                     )
                   })
                 }
               </div>
+              {/* 상품 더보기 버튼임 */}
+              <button onClick={() => {
+                //클릭 했을 때 클릭  state 에 숫자 추가 
+                클릭변경(클릭 + 1);
+                axios.get('https://codingapple1.github.io/shop/data2.json').then((결과) => {
+                  // console.log(결과.data)
+                  let copy = [...shoe, ...결과.data];
+                  shoe변경(copy);
+                })
+                  .catch(() => {
+                    console.log('실패함')
+                  })
+              }}>더보기</button>
             </div>
           </>
         }></Route>
         {/* detail 상세페이지 여러개 만들때 :id 파라미터 이용하기 */}
-        <Route path='/detail/:id' element={<Detail data = {data}/>}></Route>
+        <Route path='/detail/:id' element={<Detail data={data} />}></Route>
         {/* 404페이지 */}
         <Route path='*' element={<div>없는 페이지입니다</div>}></Route>
         {/* nested routes */}
-        <Route path='/about' element={<About/>}>
+        <Route path='/about' element={<About />}>
           <Route path='tel' element={<div>연락처 정보입니다</div>}></Route>
           <Route path='location' element={<div>위치 정보입니다</div>}></Route>
         </Route>
-        <Route path='/event' element={<Event/>}>
-           <Route path='one' element={<h4>첫 주문 시 양배추즙 서비스</h4>}></Route>
-           <Route path='two' element={<h4>생일 기념 쿠폰</h4>}></Route>
+        <Route path='/event' element={<Event />}>
+          <Route path='one' element={<h4>첫 주문 시 양배추즙 서비스</h4>}></Route>
+          <Route path='two' element={<h4>생일 기념 쿠폰</h4>}></Route>
         </Route>
       </Routes>
     </div>
   );
 }
 
+
 //About => 회사 정보 컴포넌트 
-function About(){
+function About() {
   return (
     <>
       <h3>회사 소개페이지입니다</h3>
@@ -76,8 +96,8 @@ function About(){
 }
 
 //Event => 이벤트 정보 컴포넌트 만들기
-function Event(){
-  return(
+function Event() {
+  return (
     <>
       <h3>오늘의 이벤트</h3>
       <Outlet></Outlet>
--- OneDrive/바탕 화면/리액트장인/shop/src/detail.js
+++ OneDrive/바탕 화면/리액트장인/shop/src/detail.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom"
+import { Nav } from "react-bootstrap"
 
 function Detail(props) {
 
@@ -12,35 +13,35 @@ function Detail(props) {
   })
 
   // 렌더링 2초 이후 div 사라지는 동적인 ui 구현을 위해 state 에 ui 상태값 넣어놓기
-  let[박스상태, 박스상태변경] = useState(true);
+  let [박스상태, 박스상태변경] = useState(true);
 
+  // 탭 변경 저장 state 
+  let [탭, 탭변경] = useState(0);  
 
   // component 의 라이프사이클 
   // mount / update 시마다 실행될 코드
   // 이부분에선 return 함수 사용할 필요 없음, 그냥 단순히 '박스상태변경' 이라는 함수 실행하고 끝이니까.
-  useEffect(()=>{
-      // 렌더링 2초 이후 div 사라지는 코드 
-      let a = setTimeout(()=>{     
-           박스상태변경(false);        
-      },3000,[])
+  useEffect(() => {
+    // 렌더링 2초 이후 div 사라지는 코드 
+    let a = setTimeout(() => {
+      박스상태변경(false);
+    }, 3000, [])
 
-      // clear up function
-      // ==> mount 시 실행되는 것이 아닌 unmount 시 실행됨 
-      return ()=>{
-        clearTimeout(a)
-      } 
-
+    // clear up function
+    // ==> mount 시 실행되는 것이 아닌 unmount 시 실행됨 
+    return () => {
+      clearTimeout(a)
+    }
   });
 
   return (
     <div className="container">
-      
       {
-        박스상태 == true 
-        ? <div className="alert alert-warning">
-        <p>2초 이내 구매 시 10 % 할인</p>
-      </div> 
-      : null
+        박스상태 == true
+          ? <div className="alert alert-warning">
+            <p>2초 이내 구매 시 10 % 할인</p>
+          </div>
+          : null
       }
       <div className="row">
         <div className="col-md-6">
@@ -53,7 +54,47 @@ function Detail(props) {
           <button className="btn btn-danger">주문하기</button>
         </div>
       </div>
+      <Nav variant="tabs" defaultActiveKey="link0">
+        <Nav.Item>
+          <Nav.Link onClick={()=>{탭변경(0)}} eventKey="link0">버튼0</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link onClick={()=>{탭변경(1)}} eventKey="link1">버튼1</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link onClick={()=>{탭변경(2)}} eventKey="link2">버튼2</Nav.Link>
+        </Nav.Item>
+      </Nav>
+      <TabContent 탭={탭} />
     </div>
   )
+}
+
+function TabContent({탭}){
+  // if(props.탭 == 0){
+  //   return <div className="start end">내용0</div>
+  // }else if(props.탭 == 1){
+  //   return <div>내용1</div>
+  // }else if(props.탭==2){
+  //   return <div>내용2</div>
+  // }
+
+  let [fade, setFade] = useState('')
+
+  // 탭 state 가 변경이 일어날때마다 fade 라는 state를 end 로 바꿔주세요~
+  useEffect(()=>{
+
+    setTimeout(()=>{setFade('end')},100)
+    return()=>{
+      setFade('')
+    }
+  }, [탭])
+
+  return (
+    <div className={'start ' + fade }>
+      {[<div>내용0</div>,<div>내용1</div>,<div>내용2</div>][탭]}
+    </div>
+  )
+
 }
 export default Detail
